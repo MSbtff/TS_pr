75103f1de6e01a46343b45ec7d20f19545665faf
MS_btff <whdghks845@gmail.com>
2023-06-13 01:57:04 +0900

    ts_pr

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 Object.defineProperty(exports, '__esModule', {value: true});
 var axios_1 = require('axios');
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
-axios_1.default.get(url).then(function (res) {});
+axios_1.default.get(url).then(function (res) {
+  console.log(res.data);
+});
