a875de8f0bf07f97a9fb272196e496aa819ea940
MS_btff <whdghks845@gmail.com>
2023-06-16 00:26:48 +0900

    ts_pr

--- variables.ts
+++ variables.ts
@@ -11,3 +11,19 @@ let nothing: undefined = undefined;
 
 //built in objects
 let now: Date = new Date();
+
+//Array
+let colors: string[] = ['red', 'green', 'blue']; //[]는 문자열 배열만 있다고 선언한 것
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+//Class
+class Car {}
+
+let car: Car = new Car();
+
+//Object literal
+let point: {x: number; y: number} = {
+  x: 10,
+  y: 20,
+};
