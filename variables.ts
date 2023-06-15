b86c1fb049516fbbc5f10d0aaecf2793e3cea3bd
MS_btff <whdghks845@gmail.com>
2023-06-16 00:16:47 +0900

    react_dr

---
+++ variables.ts
@@ -0,0 +1,13 @@
+let apples: number = 5; //콜론 다음에 있는 것이 타입 애너테이션(annotaition)
+
+apples = 10;
+//apples = 'asdfasfdsa'; //error
+
+let speed: string = 'fast';
+let hasNmae: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+//built in objects
+let now: Date = new Date();
